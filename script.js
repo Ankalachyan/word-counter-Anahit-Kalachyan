7cbc762c2f2d9986416da505ef5ea799c4e74c78
ankalachyan <an.kalachyan@icloud.com>
2024-07-19 13:57:14 +0400

    ready final counter app

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 function analyzeText() {
-    const text = document.getElementById('userInput').value.trim();
-    
-    let regex = /\s+g^[a-zA-Z]+$/;
-    if (regex.test(text)) { 
 
-      console.log("okay");
-        const letterCount = text.trim(/[\s\.,\/#!$%\^&\*;:{}=\-_`~()]/g, ' ').length;
-        console.log(letterCount); 
+    document.getElementById('letterCount').textContent = 0;
+    document.getElementById('wordCount').textContent = 0;
+    document.getElementById('sentenceCount').textContent = 0;
+
+   
+    const text = document.getElementById('userInput').value.trim();
+
+
+    
+    let regex = /^[a-zA-Z\s]+$/;
+    if (regex.test(text)) {
+        const letterCount = text.replace(/[^a-zA-Z]/g, '').length;
+        const wordCount = text.split(/\s+/).filter(word => word.length > 0).length;
+        const sentenceCount = text.split(/[.!?]+/).filter(sentence => sentence.trim().length > 0).length;
         
         document.getElementById('letterCount').textContent = letterCount;
-        // const wordCount = text.split(/\s+/).filter(word => word.length > 0).length;
+        document.getElementById('wordCount').textContent = wordCount;
+        document.getElementById('sentenceCount').textContent = sentenceCount;
+        document.getElementById('userInput').value = ""
 
-        // document.getElementById('wordCount').textContent = wordCount;
-
-        // const sentenceCount = text.split(/[.!?]+/).filter(sentence => sentence.trim().length > 0).length;
-
-        // document.getElementById('sentenceCount').textContent = sentenceCount;
     } else {
-        console.error('Input is not a string.');
+        alert('Input is not a string containing only letters and spaces.');
     }
-
-    console.log(text);
 }
+
 function ChangeColor() {
 
     let r = Math.floor(Math.random() * 256);
